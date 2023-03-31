3eabdacfe996e7e8fc5fd0b6a364f49257b39f5b
HDzungx <hdzungx@gmail.com>
2023-04-01 00:07:34 +0700

    munch: Add MiuiCamera Support

--- libinit/include/libinit_variant.h
+++ libinit/include/libinit_variant.h
@@ -16,7 +16,8 @@ typedef struct variant_info {
     std::string brand;
     std::string device;
     std::string marketname;
-        std::string model;
+    std::string mod_device;
+    std::string model;
     std::string build_fingerprint;
 
     bool nfc;
--- libinit/libinit_munch.cpp
+++ libinit/libinit_munch.cpp
@@ -19,6 +19,7 @@ static const variant_info_t munch_in_info = {
     .brand = "POCO",
     .device = "munch",
     .marketname = "POCO F4",
+    .mod_device = "munch_in_global",
     .model = "22021211RI",
     .build_fingerprint = FINGERPRINT_IN,
 
@@ -31,6 +32,7 @@ static const variant_info_t munch_global_info = {
     .brand = "POCO",
     .device = "munch",
     .marketname = "POCO F4",
+    .mod_device = "munch_global",
     .model = "22021211RG",
     .build_fingerprint = FINGERPRINT_GL,
     
@@ -43,6 +45,7 @@ static const variant_info_t munch_info = {
     .brand = "Redmi",
     .device = "munch",
     .marketname = "Redmi K40S",
+    .mod_device = "munch_global",
     .model = "22021211RC",
     .build_fingerprint = FINGERPRINT_CN,
     
