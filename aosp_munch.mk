1dfdf66bfbafcd2c5c33da42ac338772e9b4240e
Mudit200408 <muditpurohit2004.pm@gmail.com>
2023-09-30 19:15:30 +0530

    munch: Update blobs to V14.0.4.0.TLMMIXM

--- aosp_munch.mk
+++ aosp_munch.mk
@@ -40,4 +40,4 @@ PRODUCT_MODEL := POCO F4
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Redmi/munch/munch:13/RKQ1.211001.001/V14.0.3.0.TLMMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/munch/munch:13/RKQ1.211001.001/V14.0.4.0.TLMMIXM:user/release-keys
