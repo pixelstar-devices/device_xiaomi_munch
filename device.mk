198ab671d209dd1d835bedbd3b7f7bc0ce20d372
Mudit200408 <muditpurohit2004.pm@gmail.com>
2023-09-30 23:28:12 +0530

    munch: Add the remaining CAF Framework Boost libs

--- device.mk
+++ device.mk
@@ -612,6 +612,10 @@ PRODUCT_BOOT_JARS += \
     QPerformance \
     UxPerformance
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0
+
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
 
