221be26040e3db21d959b9bfd83eb258a94b3c03
Mudit200408 <muditpurohit2004.pm@gmail.com>
2024-01-24 14:19:09 +0530

    munch: pixelstar bring-up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_munch.mk
+    $(LOCAL_DIR)/pixelstar_munch.mk
 
 COMMON_LUNCH_CHOICES := \
-    cherish_munch-user \
-    cherish_munch-userdebug \
-    cherish_munch-eng
+    pixelstar_munch-user \
+    pixelstar_munch-userdebug \
+    pixelstar_munch-eng
---
+++ pixelstar_munch.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/pixelstar/config/common_full_phone.mk)
+
+# Inherit from munch device
+$(call inherit-product, device/xiaomi/munch/device.mk)
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier
+PRODUCT_NAME := pixelstar_munch
+PRODUCT_DEVICE := munch
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO F4
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Specific flags
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+PIXELSTAR_BUILD_TYPE := official
+USE_PIXEL_CHARGER := true
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
