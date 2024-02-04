d1540383484b84512cc8cf6071ab8530eee01f02
HDzungx <hdzungx@gmail.com>
2024-02-04 21:19:52 +0700

    munch: Add clang flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,6 +103,13 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_CONFIG := munch_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
 
+# Kernel Clang
+TARGET_KERNEL_CLANG_COMPILE := true
+KERNEL_LD := LD=ld.lld
+TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
+TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1 LLVM_IAS=1
+TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+
 # Miui Camera
 TARGET_INCLUDES_MIUI_CAMERA := true
 
